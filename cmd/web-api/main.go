76fd177da212a439945da64b90360a10a940b6e0
mitra <mitra.j@780.ir>
2024-02-17 02:13:57 +0330

    Enhance application configuration and initialization:
    - Introduce dynamic port configuration using environment variables.
    - Implement environment variable loading from the .env file in the Docker folder.
    - Create a loadEnvFile function for cleaner and modular code.
    - Update server address configuration in main.go to use the LISTEN_PORT environment variable.
    - Strengthen error handling during initialization by logging fatal errors.
    - Adjust Docker environment variables in docker/.env file.

--- cmd/web-api/main.go
+++ cmd/web-api/main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"interview/pkg/controllers"
 	"interview/pkg/db"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
+func loadEnvFile() error {
+	// Construct the absolute path to the .env file in the Docker folder
+	envFilePath := filepath.Join("../..", "docker", ".env")
+
+	// Load environment variables from .env file
+	if err := godotenv.Load(envFilePath); err != nil {
+		return err
+	}
+
+	log.Println(".env file loaded successfully")
+	return nil
+}
+
 func main() {
+	if err := loadEnvFile(); err != nil {
+		log.Fatalf("Error during initialization: %v", err)
+	}
+
 	db.MigrateDatabase()
 
 	ginEngine := gin.Default()
@@ -17,7 +38,7 @@ func main() {
 	ginEngine.POST("/add-item", cartController.AddItem)
 	ginEngine.GET("/remove-cart-item", cartController.DeleteCartItem)
 	srv := &http.Server{
-		Addr:    ":8088",
+		Addr:    ":" + os.Getenv("LISTEN_PORT"),
 		Handler: ginEngine,
 	}
 

5e9bca81a27cf20d7421d2c80d049ebaa0336ca6
mitra <mitra.j@780.ir>
2024-02-17 11:03:46 +0330

    Enhancement AddItem functionality in CartController and Service
    - Refactor AddItem method in CartController to handle form validation and error messages.
    - Introduce a CartItemForm struct for binding request parameters in AddItem.
    - Update AddItemToCart function in service to accept sessionID, product, and quantity as parameters.
    - Use constants for error messages and map item prices in the service.
    - Improve error handling and readability in both CartController and service.

--- pkg/controller/add_item.go
+++ pkg/controller/add_item.go
@@ -3,17 +3,41 @@ package controller
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"interview/pkg/service"
 	"net/http"
+	"strconv"
 )
 
 func (cc *CartController) AddItem(c *gin.Context) {
-	cookie, err := c.Request.Cookie("ice_session_id")
+	cookie, err := c.Request.Cookie(ICE_SESSION_ID)
 
 	if err != nil || errors.Is(err, http.ErrNoCookie) || (cookie != nil && cookie.Value == "") {
 		cc.RedirectTo(c, "/")
 		return
 	}
 
-	service.AddItemToCart(c)
+	if c.Request.Body == nil {
+		cc.RedirectTo(c, "/?error="+"body cannot be nil")
+		return
+	}
+
+	form := &service.CartItemForm{}
+	if err := binding.FormPost.Bind(c.Request, form); err != nil {
+		cc.RedirectTo(c, "/?error="+err.Error())
+		return
+	}
+
+	quantity, err := strconv.ParseInt(form.Quantity, 10, 0)
+	if err != nil {
+		cc.RedirectTo(c, "/?error=invalid quantity")
+		return
+	}
+
+	err = service.AddItemToCart(cookie.Value, form.Product, quantity)
+	if err != nil {
+		cc.RedirectTo(c, "/?error="+err.Error())
+		return
+	}
+	cc.RedirectTo(c, "/")
 }
--- pkg/service/add_item.go
+++ pkg/service/add_item.go
@@ -3,13 +3,9 @@ package service
 import (
 	"errors"
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/binding"
 	"gorm.io/gorm"
 	"interview/pkg/entity"
 	db2 "interview/pkg/repository"
-	"log"
-	"strconv"
 )
 
 var itemPriceMapping = map[string]float64{
@@ -24,92 +20,52 @@ type CartItemForm struct {
 	Quantity string `form:"quantity"  binding:"required"`
 }
 
-func AddItemToCart(c *gin.Context) {
-	cookie, _ := c.Request.Cookie("ice_session_id")
+func AddItemToCart(sessionID string, product string, quantity int64) error {
 
+	itemPrice, ok := itemPriceMapping[product]
+	if !ok {
+		return errors.New("invalid item name")
+	}
 	db := db2.GetDatabase()
-
-	var isCartNew bool
 	var cartEntity entity.CartEntity
-	result := db.Where(fmt.Sprintf("status = '%s' AND session_id = '%s'", entity.CartOpen, cookie.Value)).First(&cartEntity)
-
+	var cartItemEntity entity.CartItem
+	result := db.Where(fmt.Sprintf("status = '%s' AND session_id = '%s'", entity.CartOpen, sessionID)).First(&cartEntity)
 	if result.Error != nil {
 		if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			c.Redirect(302, "/")
-			return
+			return result.Error
 		}
-		isCartNew = true
 		cartEntity = entity.CartEntity{
-			SessionID: cookie.Value,
+			SessionID: sessionID,
 			Status:    entity.CartOpen,
 		}
 		db.Create(&cartEntity)
-	}
-
-	addItemForm, err := getCartItemForm(c)
-	if err != nil {
-		c.Redirect(302, "/?error="+err.Error())
-		return
-	}
-
-	item, ok := itemPriceMapping[addItemForm.Product]
-	if !ok {
-		c.Redirect(302, "/?error=invalid item name")
-		return
-	}
-
-	quantity, err := strconv.ParseInt(addItemForm.Quantity, 10, 0)
-	if err != nil {
-		c.Redirect(302, "/?error=invalid quantity")
-		return
-	}
-
-	var cartItemEntity entity.CartItem
-	if isCartNew {
 		cartItemEntity = entity.CartItem{
 			CartID:      cartEntity.ID,
-			ProductName: addItemForm.Product,
+			ProductName: product,
 			Quantity:    int(quantity),
-			Price:       item * float64(quantity),
+			Price:       itemPrice * float64(quantity),
 		}
 		db.Create(&cartItemEntity)
 	} else {
-		result = db.Where(" cart_id = ? and product_name  = ?", cartEntity.ID, addItemForm.Product).First(&cartItemEntity)
+		result = db.Where(" cart_id = ? and product_name  = ?", cartEntity.ID, product).First(&cartItemEntity)
 
 		if result.Error != nil {
 			if !errors.Is(result.Error, gorm.ErrRecordNotFound) {
-				c.Redirect(302, "/")
-				return
+				return result.Error
 			}
 			cartItemEntity = entity.CartItem{
 				CartID:      cartEntity.ID,
-				ProductName: addItemForm.Product,
+				ProductName: product,
 				Quantity:    int(quantity),
-				Price:       item * float64(quantity),
+				Price:       itemPrice * float64(quantity),
 			}
 			db.Create(&cartItemEntity)
-
 		} else {
 			cartItemEntity.Quantity += int(quantity)
-			cartItemEntity.Price += item * float64(quantity)
+			cartItemEntity.Price += itemPrice * float64(quantity)
 			db.Save(&cartItemEntity)
 		}
 	}
 
-	c.Redirect(302, "/")
-}
-
-func getCartItemForm(c *gin.Context) (*CartItemForm, error) {
-	if c.Request.Body == nil {
-		return nil, fmt.Errorf("body cannot be nil")
-	}
-
-	form := &CartItemForm{}
-
-	if err := binding.FormPost.Bind(c.Request, form); err != nil {
-		log.Println(err.Error())
-		return nil, err
-	}
-
-	return form, nil
+	return nil
 }
